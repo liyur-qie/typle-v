e4cfaf74a400ee85367757e9b8dc872f3f888ad8
Liyur-xue <liyur@outlook.jp>
2024-05-12 02:18:13 +0900

    基本構造をマークアップ

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,75 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <nav>
+    <h1>Typle</h1>
+    <ul>
+      <li><a href="">Home</a></li>
+      <li><a href="">Play</a></li>
+      <li><a href="">Rankings</a></li>
+      <li><a href="">Cretae</a></li>
+      <li><a href="">Edit</a></li>
+      <li><a href="">About</a></li>
+    </ul>
+  </nav>
+  <main>
+    <section id="playArea">
+      <div id="wordDisplay">Hello, World</div>
+      <input type="text" class="wordInputField" />
+      <section id="wordList">
+        <h1>Words</h1>
+        <span>hoge</span>
+        <span>foo</span>
+        <span>bar</span>
+        <span>vue</span>
+        <span>react</span>
+      </section>
+    </section>
+    <section id="rankings">
+      <table>
+        <tr>
+          <td>No.</td>
+          <td>Time</td>
+          <td>Words</td>
+          <td>Date</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>12seconds</td>
+          <td>5 words</td>
+          <td>{{ new Date().toLocaleString() }}</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>12seconds</td>
+          <td>5 words</td>
+          <td>{{ new Date().toLocaleString() }}</td>
+        </tr>
+      </table>
+    </section>
+    <section id="selectWordList">
+      <table>
+        <tr>
+          <td>
+            <span>Name</span>
+          </td>
+          <td>
+            <span>Length</span>
+          </td>
+          <td>
+            <span>Action</span>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <span>Vue 3</span>
+          </td>
+          <td>
+            <span>4 words</span>
+          </td>
+          <td>
+            <button>Select</button>
+          </td>
+        </tr>
+      </table>
+    </section>
+  </main>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
