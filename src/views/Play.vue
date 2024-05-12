5edd444245ba782d73136464ce9174cb9bcbbc27
liyur-qie <liyur@outlook.jp>
2024-05-13 03:05:21 +0900

    移動し忘れたコードを移動

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -1,3 +1,125 @@
 <template>
-  <h1>Play.vue</h1>
+<section id="playArea">
+      <div
+        id="wordDisplay"
+        class="bg-gray-900 text-white flex justify-center items-center text-4xl h-28"
+      >Hello, World</div>
+      <input
+        type="text"
+        id="wordInputField"
+        class="bg-gray-800 text-white text-center w-full text-2xl font-light h-28 outline-none"
+        placeholder="Type the word above here"
+        />
+    </section>
+    <div class="w-4/5 mx-auto">
+      <section id="wordList" class="mt-6">
+        <h1 class="text-xl my-3">Selected: {{ wordLists[0].name }}</h1>
+        <span
+          v-for="(word, index) in wordLists[0].words"
+          :key="index"
+          class="text-sm mr-2 py-1 px-2 bg-gray-200"
+        >
+          {{ word.wordDisplayed }}
+        </span>
+      </section>
+      <section id="rankings" class="mt-6">
+        <table>
+          <tr>
+            <td class="text-sm text-gray-700">No.</td>
+            <td class="text-sm text-gray-700">Time</td>
+            <td class="text-sm text-gray-700">Words</td>
+            <td class="text-sm text-gray-700">Date</td>
+          </tr>
+          <tr v-for="(record, index) in rankingRecords">
+            <td>{{ index + 1 }}</td>
+            <td>{{ record.time }} seconds</td>
+            <td>{{ record.length }} words</td>
+            <td>{{ record.date }}</td>
+          </tr>
+        </table>
+      </section>
+      <section id="selectWordList">
+        <table>
+          <tr>
+            <td>
+              <span>Name</span>
+            </td>
+            <td>
+              <span>Length</span>
+            </td>
+            <td>
+              <span>Action</span>
+            </td>
+          </tr>
+          <tr v-for="(wordList, index) in wordLists">
+            <td :key="index">
+              <span>{{ wordList.name }}</span>
+            </td>
+            <td :key="index">
+              <span>{{ wordList.words.length }} words</span>
+            </td>
+            <td :key="index">
+              <button>PLAY</button>
+            </td>
+          </tr>
+        </table>
+      </section>
+    </div>
 </template>
+<script setup lang="ts">
+type RankingRecord = {
+  time: number | Date,
+  length: number,
+  date: string | Date
+}
+const rankingRecords: RankingRecord[] = [
+  { time: 4, length: 3, date: new Date().toLocaleString() },
+  { time: 5, length: 3, date: new Date().toLocaleString() },
+  { time: 6, length: 3, date: new Date().toLocaleString() },
+]
+
+type WordListWord = {
+  wordDisplayed: string,
+  wordExpectedToInput: string,
+  annotation?: string,
+}
+
+type WordListInfo = {
+  createdAt: string | Date,
+  createdBy?: string,
+  updatedAt?: string | Date,
+  
+}
+
+type WordList = {
+  name: string,
+  description?: string
+  words: WordListWord[],
+  rankingRecords: RankingRecord[],
+  info: WordListInfo
+}
+const wordLists: WordList[] = [
+  {
+    name: "Example 1",
+    description: "This is an example word list",
+    words: [
+      {
+        wordDisplayed: "hoge",
+        wordExpectedToInput: "hoge",
+      },
+      {
+        wordDisplayed: "foo",
+        wordExpectedToInput: "foo",
+      },
+      {
+        wordDisplayed: "bar",
+        wordExpectedToInput: "bar",
+      },
+    ],
+    rankingRecords: rankingRecords,
+    info: {
+      createdAt: new Date().toLocaleString(),
+    }
+  }
+]
+</script>
