eb0c8c3331448f1c6f90493ea1657069058d10fa
liyur-qie <liyur@outlook.jp>
2024-06-02 20:39:22 +0900

    チップとテーブルをvuetifyに移行

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -1,70 +1,61 @@
 <template>
-<section id="playArea">
-      <div
-        id="wordDisplay"
-        class="bg-gray-900 text-white flex justify-center items-center text-4xl h-28"
-      >Hello, World</div>
-      <input
-        type="text"
-        id="wordInputField"
-        class="bg-gray-800 text-white text-center w-full text-2xl font-light h-28 outline-none"
-        placeholder="Type the word above here"
-        />
+  <section id="playArea">
+    <div
+      id="wordDisplay"
+      class="bg-gray-900 text-white flex justify-center items-center text-4xl h-28"
+    >Hello, World</div>
+    <input
+      type="text"
+      id="wordInputField"
+      class="bg-gray-800 text-white text-center w-full text-2xl font-light h-28 outline-none"
+      placeholder="Type the word above here"
+      />
+  </section>
+  <div class="w-4/5 mx-auto">
+    <section id="wordList" class="mt-6">
+      <h1 class="text-xl my-3">Selected: {{ wordList.name }}</h1>
+      <v-chip
+        v-for="(word, index) in wordLists[0].words"
+        :key="index"
+        class="text-sm mr-2 py-1 px-2 bg-gray-200"
+      >
+        {{ word.wordDisplayed }}
+      </v-chip>
     </section>
-    <div class="w-4/5 mx-auto">
-      <section id="wordList" class="mt-6">
-        <h1 class="text-xl my-3">Selected: {{ wordList.name }}</h1>
-        <span
-          v-for="(word, index) in wordLists[0].words"
-          :key="index"
-          class="text-sm mr-2 py-1 px-2 bg-gray-200"
-        >
-          {{ word.wordDisplayed }}
-        </span>
-      </section>
-      <section id="rankings" class="mt-6">
-        <table>
-          <tr>
-            <td class="text-sm text-gray-700">No.</td>
-            <td class="text-sm text-gray-700">Time</td>
-            <td class="text-sm text-gray-700">Words</td>
-            <td class="text-sm text-gray-700">Date</td>
-          </tr>
-          <tr v-for="(record, index) in wordList.records">
-            <td>{{ index + 1 }}</td>
-            <td>{{ record.time }} seconds</td>
-            <td>{{ record.length }} words</td>
-            <td>{{ record.date }}</td>
-          </tr>
-        </table>
-      </section>
-      <section id="selectWordList">
-        <table>
-          <tr>
-            <td>
-              <span>Name</span>
-            </td>
-            <td>
-              <span>Length</span>
-            </td>
-            <td>
-              <span>Action</span>
-            </td>
-          </tr>
-          <tr v-for="(wordList, index) in wordLists">
-            <td :key="index">
-              <span>{{ wordList.name }}</span>
-            </td>
-            <td :key="index">
-              <span>{{ wordList.words.length }} words</span>
-            </td>
-            <td :key="index">
-              <button>PLAY</button>
-            </td>
-          </tr>
-        </table>
-      </section>
-    </div>
+    <section id="rankings" class="mt-6">
+      <v-data-table-virtual
+        :headers="headers"
+        :items="wordList.records"
+        item-value="name"
+      ></v-data-table-virtual>
+    </section>
+    <section id="selectWordList">
+      <table>
+        <tr>
+          <td>
+            <span>Name</span>
+          </td>
+          <td>
+            <span>Length</span>
+          </td>
+          <td>
+            <span>Action</span>
+          </td>
+        </tr>
+        <tr v-for="(wordList, index) in wordLists">
+          <td :key="index">
+            <span>{{ wordList.name }}</span>
+          </td>
+          <td :key="index">
+            <span>{{ wordList.words.length }} words</span>
+          </td>
+          <td :key="index">
+            <button>PLAY</button>
+          </td>
+        </tr>
+      </table>
+    </section>
+  </div>
 </template>
 <script setup lang="ts">
 import { WordList } from '@/models/screen/WordList';
@@ -98,5 +89,12 @@ wordLists.push(exampleWordList)
 
 const wordListIndex = ref(0)
 const wordList = computed(() => wordLists[wordListIndex.value])
+
+const headers = [
+  { title: "No.", align: "start", key: "index" },
+  { title: "Time", align: "start", key: "time" },
+  { title: "Words", align: "start", key: "length" },
+  { title: "Date", align: "start", key: "date" },
+]
 
 </script>
