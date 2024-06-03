18c706bbcabab1bf82ae9f630806f01195dbcd3d
liyur-qie <liyur@outlook.jp>
2024-06-03 21:35:47 +0900

    Typography.vueを作成

---
+++ src/components/Typography/Typography.vue
@@ -0,0 +1,18 @@
+<template>
+  <p :class="classNames">
+    <slot />
+  </p>
+</template>
+<script setup lang="ts">
+import { computed } from '@vue/reactivity';
+
+const props = defineProps<{
+  size?: "text-xs" | "text-sm" | "text-base" | "text-lg" | "text-xl" | "text-2xl"
+}>()
+
+const classNames = computed(() => {
+  if(props.size !== undefined){
+    return props.size
+  }
+}
+</script>
